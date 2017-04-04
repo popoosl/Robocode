8b086c3d80017d8f11c70035ab2968abb113a0f4
sju88 <sju2@ncsu.edu>
2017-04-03 21:53:48 -0400

    add cluster prediction in radar

--- src/MyRobots/MyRobot.java
+++ src/MyRobots/MyRobot.java
@@ -8,6 +8,7 @@ import robocode._AdvancedRadiansRobot;
 import robocode.*;
 import robocode.util.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.util.*;
 
 
@@ -24,6 +25,12 @@ public class MyRobot extends AdvancedRobot {
 	HashMap<String, TankBot> enemies = new HashMap<String, TankBot>();
 	// Radar State
 	int radarStrategy = CIRCLESCAN;
+	// Enemy Distributio
+	// 0 means there is only 1 enemy or the enemies distribute around character equally.
+	// There are 4 area. 1:front, (-45,45); 2:right, (45,135); 3:behind, (-135,-135); 4:left, (-45,-135)  
+	int enemyDistribution = 0;
+	
+	double enemyDistributionThreshold = 500;
 	int turnCount = 0;
 
 	public void run() {
@@ -65,11 +72,74 @@ public class MyRobot extends AdvancedRobot {
 	}
 	
 	public void updateStrategy(){
-		// Use first 20 turns to scan 360-degree, make sure there's no enemy miss
+		// Update radar strategy: Use first 20 turns to scan 360-degree, make sure there's no enemy miss
 		if(enemies.size()==1 && turnCount > 20)
 			radarStrategy = LOCK;
 		else
 			radarStrategy = CIRCLESCAN;
+		
+		// Update enemy state
+		enemyDistribution = updateEnemyState();
+		if(enemyDistribution == 0){
+			out.println("Sparse!");
+		}
+		else
+			out.println("enemy concentrated in: " + enemyDistribution);
+	}
+	
+	public int updateEnemyState(){
+		// If there's only 1 enemy, return 0
+		if(enemies.size() <= 1){
+			return 0;
+		}
+		
+		int AREA[] = new int[5];
+		Arrays.fill(AREA, 0);
+		for(TankBot t : enemies.values()){
+			double bearing = t.bearing;
+//			out.println("Name: " + t.name + "  bear: " + bearing);
+			if(-45 <= bearing && bearing <= 45)
+				AREA[1]++;
+			if(45 <= bearing && bearing <= 135)
+				AREA[2]++;
+			if((135 <= bearing && bearing <= 180) || (-180 <= bearing && bearing <= -135))
+				AREA[3]++;
+			if(-135 <= bearing && bearing <= -45)
+				AREA[4]++;
+		}
+		
+//		out.println(AREA[1] + " - " + AREA[2] + " - " + AREA[3] + " - " + AREA[4]);
+		
+		int max = 0;
+		int index = 0;
+		for(int i = 1; i < AREA.length; i++){
+			if(AREA[i] > max){
+				max = AREA[i];
+				index = i;
+			}
+				
+		}
+		
+		if(max > enemies.size()/4 + 1)
+			return index;
+		else
+			return 0;
+		
+		// If the vector is larger than threshold, the enemy is concentrate and return the mean center
+		// else return null
+//		if(Math.sqrt(X*X + Y*Y) > enemyDistributionThreshold){
+//			enemyState = ENEMYCONCENTRATE;
+//			Point2D.Double p = new Point2D.Double();
+//			int c = enemies.size();
+//			p.x = X/c + this.getX();
+//			p.y = Y/c + this.getY();
+//			return p;
+//		}
+//		else{
+//			enemyState = ENEMYSPARSE;
+//			return null;
+//		}
+
 	}
 
 	void Radar() {
